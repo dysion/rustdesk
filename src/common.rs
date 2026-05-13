45a3f97c45ff05bfec230e100684b7310e78fae5
dysion <support@dysion.cn>
2026-05-13 17:05:36 +0800

    Critical: 修改BUG

--- src/common.rs
+++ src/common.rs
@@ -1054,10 +1054,10 @@ pub fn get_api_server(api: String, custom: String) -> String {
         res.pop();
     }
     if res.starts_with("https")
-        && res.ends_with(":21114")
-        && get_builtin_option(keys::OPTION_ALLOW_HTTPS_21114) != "Y"
+        && res.ends_with(":1114")
+        && get_builtin_option(keys::OPTION_ALLOW_HTTPS_1114) != "Y"
     {
-        return res.replace(":21114", "");
+        return res.replace(":1114", "");
     }
     res
 }
